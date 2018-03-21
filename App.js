d44f078a5ebc85f8bb07bfbea8dba185750be445
wuxiaojuan <wuxiaojuan@4399inc.com>
2018-03-21 15:31:41 +0800

    feat: app

---
+++ App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import {
+  Platform,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+import { Button } from 'antd-mobile';
+
+export default class App extends Component {
+  render() {
+    return <Button>Starts</Button>;
+  }
+}
